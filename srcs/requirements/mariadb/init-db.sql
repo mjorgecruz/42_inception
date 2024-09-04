f23963ae21bb60a852a2e82a91f33278241e84c2
Jorge Cruz <manueljorgecruz@hotmail.com>
2024-09-04 15:37:07 +0100

    database done

---
+++ srcs/requirements/mariadb/init-db.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE USER 'wp_user'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_user'@'localhost';
+FLUSH PRIVILEGES
