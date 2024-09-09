df903ef65bb657b32c09572467c24ef51068ec38
Jorge Cruz <manueljorgecruz@hotmail.com>
2024-09-10 00:16:20 +0100

    lel

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -13,7 +13,7 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod 777 wp-cli.phar
 sleep 5
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wp_user --dbpass=password --dbhost=localhost --allow-root
+./wp-cli.phar config create --dbname=wordpress --dbuser=wp_user --dbpass=password --dbhost=mariadb --allow-root
 ./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
 ./wp-cli.phar user create guest guest@guest.com --role=subscriber --user_pass=guest123 --allow-rootphp
 chown -R www-data:www-data /var/www/html/wp-content/uploads
