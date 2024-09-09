c9ba924b927922901717f54e7501bda12d457a6c
Jorge Cruz <manueljorgecruz@hotmail.com>
2024-09-10 00:57:52 +0100

    ainda não cria ficheiro wp-config

--- srcs/requirements/mariadb/init-db.sh
+++ srcs/requirements/mariadb/init-db.sh
@@ -3,9 +3,10 @@
 cat << EOF > /etc/mysql/init.sql
 
 CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-CREATE USER 'wp_user'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_user'@'localhost';
+CREATE USER 'wp_user'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_user'@'%';
 FLUSH PRIVILEGES;
 EOF
+chmod 777 /etc/mysql/init.sql
 mysql install_db
 mysqld --init-file=/etc/mysql/init.sql
