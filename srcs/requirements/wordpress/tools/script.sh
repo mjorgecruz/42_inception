9d28008b3681d72e351df27819c6f55b7523cbe4
Jorge Cruz <manueljorgecruz@hotmail.com>
2024-09-10 23:54:22 +0100

    I think it is working

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -15,8 +15,8 @@ sleep 5
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=wordpress --dbuser=wp_user --dbpass=password --dbhost=mariadb --allow-root
 ./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
-./wp-cli.phar user create guest guest@guest.com --role=subscriber --user_pass=guest123 --allow-rootphp
+./wp-cli.phar user create guest guest@guest.com --role=subscriber --user_pass=guest123 --allow-root
 chown -R www-data:www-data /var/www/html/wp-content/uploads
 chown -R www-data:www-data /var/www/html
-# echo "define('FS_METHOD', 'direct');" >> /var/www/html/wp-config.php
+echo "define('FS_METHOD', 'direct');" >> /var/www/html/wp-config.php
 php-fpm7.4 -F
