e8206359b5d5d0b6836de006c58ee7d52366b7fc
Jorge Cruz <manueljorgecruz@hotmail.com>
2024-09-11 09:52:39 +0100

    aqui vai

--- Makefile
+++ Makefile
@@ -18,3 +18,9 @@ clean:
 	docker volume prune -a 
 	docker volume rm srcs_wordpress_data srcs_mariadb_data
 	rm -rf /home/$(USER)/data;
+
+fclean: 
+	docker stop $(docker ps -qa); docker rm $(docker ps -qa)
+	docker rmi -f $(docker images -qa)
+	docker volume rm $(docker volume ls -q)
+	docker network rm $(docker network ls -q) 2>/dev/null^C
--- srcs/requirements/mariadb/init-db.sh
+++ srcs/requirements/mariadb/init-db.sh
@@ -2,11 +2,13 @@
 
 cat << EOF > /etc/mysql/init.sql
 
-CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-CREATE USER 'wp_user'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_user'@'%';
+CREATE DATABASE ${DATABASE_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE USER '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_PW}';
+GRANT ALL PRIVILEGES ON wordpress.* TO '${DATABASE_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
-chmod 777 /etc/mysql/init.sql
+
+chmod +x /etc/mysql/init.sql
+
 mysql install_db
 mysqld --init-file=/etc/mysql/init.sql
